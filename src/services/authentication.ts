c86662f3aa0c0cf5213ed1d123b065a6bc426a87
Beatriz <beatrizamante@hotmail.com>
2024-12-03 21:33:49 -0300

    URL

--- src/services/authentication.ts
+++ src/services/authentication.ts
@@ -1,20 +1,20 @@
 export const handLogin = async (username: string, password: string): Promise<boolean> => {
-  const API_URL = process.env.REACT_APP_API_URL;
+  const API_URL = `${process.env.REACT_APP_API_URL}:3000`;;
 
   try {
     const response = await fetch(`${API_URL}/login`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ university_registry: username, password }), // Alinhando com o back
+      body: JSON.stringify({ university_registry: username, password }), 
     });
 
-    const result = await response.text(); // Captura a mensagem de resposta do back como texto
+    const result = await response.text(); 
 
     if (response.ok && result.includes("user logged in successfully")) {
       console.log("Login bem-sucedido!"); 
       return true; 
     } else {
-      console.error("Erro no login:", result); // Loga a mensagem de erro do back
+      console.error("Erro no login:", result); 
       return false;
     }
 
