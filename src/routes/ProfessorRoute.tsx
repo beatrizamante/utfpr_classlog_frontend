31d7a6e58817770e0c4fcba0f65ed78605da6924
Beatriz <beatrizamante@hotmail.com>
2025-02-25 00:25:14 -0300

    All changes on css

--- src/routes/ProfessorRoute.tsx
+++ src/routes/ProfessorRoute.tsx
@@ -1,7 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import ProfessorPage from "../pages/Professors";
-import ListarMaterias from "../pages/Professors/ListarMaterias";
-import ListarSalas from "../pages/Professors/ListarSalas";
 import CancelSchedule from "../pages/Professors/CancelSchedule";
 import CancelScheduleId from "../pages/Professors/CancelScheduleId";
 import ChangeRoom from "../pages/Professors/ChangeRoom";
@@ -14,12 +12,6 @@ const ProfessorRoute: React.FC = () => (
       <Route path="/change-room" element={<ChangeRoom />} />
       <Route path="/change-room/:id" element={<ChangeRoomId />} />
       <Route path="/cancelar-aula/:id" element={<CancelScheduleId />} />
-
-    <Route path="curso/materia">
-      <Route index element={<ListarMaterias />} />
-      <Route path=":subjectId/sala" element={<ListarSalas />} />
-    </Route>
-
     <Route path="*" element={<Navigate to="/" />} />
   </Routes>
 );
